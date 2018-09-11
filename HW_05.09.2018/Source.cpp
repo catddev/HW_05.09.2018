a43d06edddb8d45b56c54d9ec33ce5b3975caae5
vs <vs@itstep.kz>
2018-09-11 15:20:49 +0600

    -

--- HW_05.09.2018/Source.cpp
+++ HW_05.09.2018/Source.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <iomanip>
 #include<math.h>
 #include<ctime>
 
 using namespace std;
 
-// 1.	Написать программу, которая удаляет из двухмерного массива, заполненного случайными числами, строки, содержащие нулевые элементы.
+// 1.	РќР°РїРёСЃР°С‚СЊ РїСЂРѕРіСЂР°РјРјСѓ, РєРѕС‚РѕСЂР°СЏ СѓРґР°Р»СЏРµС‚ РёР· РґРІСѓС…РјРµСЂРЅРѕРіРѕ РјР°СЃСЃРёРІР°, Р·Р°РїРѕР»РЅРµРЅРЅРѕРіРѕ СЃР»СѓС‡Р°Р№РЅС‹РјРё С‡РёСЃР»Р°РјРё, СЃС‚СЂРѕРєРё, СЃРѕРґРµСЂР¶Р°С‰РёРµ РЅСѓР»РµРІС‹Рµ СЌР»РµРјРµРЅС‚С‹.
 void delete_str(int**&a, int &n, int m) {
 	for (int pass = 0; pass < n - 1; pass++)
 	{
@@ -48,16 +48,120 @@ void delete_str(int**&a, int &n, int m) {
 	cout << endl;
 }
 
+// 2 case 2
 
+// 3.	РЎРѕР·РґР°Р№С‚Рµ РґРёРЅР°РјРёС‡РµСЃРєРёР№ РјР°СЃСЃРёРІ, С…СЂР°РЅСЏС‰РёР№ РІ РїРµСЂРІРѕР№ СЃС‚СЂРѕРєРµ РёРґРµС‚РёС„РёРєР°С†РёРѕРЅРЅС‹Р№ РЅРѕРјРµСЂ, Р° РІРѕ РІС‚РѕСЂРѕР№ - С‚РµР»РµС„РѕРЅ.
+//РћСЂРіР°РЅРёР·СѓР№С‚Рµ РїРѕРёСЃРє РїРѕ ID Рё РїРѕ РЅРѕРјРµСЂСѓ С‚РµР»РµС„РѕРЅР° Рё РІРѕР·РјРѕР¶РЅРѕСЃС‚СЊ РІРІРѕРґР° Рё РёР·РјРµРЅРµРЅРёСЏ РґР°РЅРЅС‹С….
 
+void input(int **&a, int &n) {
+	int id, nomer;
+	cin >> id >> nomer;
 
+	int **tmp;
+	tmp = new int*[2];
+	tmp[0] = new int[n + 1];
+	tmp[1] = new int[n + 1];
 
+	for (int i = 0; i < n; i++)
+	{
+		tmp[0][i] = a[0][i];
+		tmp[1][i] = a[1][i];
+	}
+	tmp[0][n] = id;
+	tmp[1][n] = nomer;
 
+	if (n != 0)
+	{
+		delete[] a[0], delete[] a[1];
+		delete[] a;
+	}
+	a = tmp;
+	n++;
+}
+void change(int **&a, int n) {
+	cout << "РІРІРµРґРёС‚Рµ РёРЅРґРµРєСЃ РЅРѕРјРµСЂР°, РєРѕС‚РѕСЂС‹Р№ С…РѕС‚РёС‚Рµ РёР·РјРµРЅРёС‚СЊ (РёР»Рё СЃРЅР°С‡Р°Р»Р° РІРѕСЃРїРѕР»СЊР·СѓР№С‚РµСЃСЊ С„СѓРЅРєС†РёРµР№ РїРѕРёСЃРєР°):" << endl;
+	int k;
+	cin >> k;
 
-// 3.	Создайте динамический массив, хранящий в первой строке идетификационный номер, а во второй - телефон.
-//Организуйте поиск по ID и по номеру телефона и возможность ввода и изменения данных.
+	cout << "РІРІРµРґРёС‚Рµ РЅРѕРІС‹Р№ ID Рё РЅРѕРјРµСЂ:" << endl;
+	int id, nomer;
+	cin >> id >> nomer;
 
+	int **tmp;
+	tmp = new int*[2];
+	tmp[0] = new int[n];
+	tmp[1] = new int[n];
 
+	for (int i = 0; i < n; i++)
+	{
+		tmp[0][i] = a[0][i];
+		tmp[1][i] = a[1][i];
+		if (i == k)
+		{
+			tmp[0][k] = id;
+			tmp[1][k] = nomer;
+		}
+	}
+	
+	if (n != 0)
+	{
+		delete[] a[0], delete[] a[1];
+		delete[] a;
+	}
+	a = tmp;
+}
+bool searchID(int **a, int n) {
+	int id;
+	cin >> id;
+
+	int k = -1;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[0][i] == id) k = i;
+	}
+	if (k < 0)
+	{
+		cout << "number (" << id << ") is not found" << endl;
+		return false;
+	}
+	else
+	{
+		cout << "id: "<< id <<" is under the index: " << k << ", tel number is: " << a[1][k] << endl;
+		return true;
+	}
+}
+bool searchNum(int **a, int n) {
+	int nomer;
+	cin >> nomer;
+
+	int k = -1;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[1][i] == nomer) k = i;
+	}
+	if (k < 0)
+	{
+		cout << "number (" << nomer << ") is not found" << endl;
+		return false;
+	}
+	else
+	{
+		cout << "telephone number ( " << nomer << ") is under the index: " << k << ", id: " << a[0][k] << endl;
+		return true;
+	}
+}
+void print(int **a, int n) {
+	cout << endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << "   ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
 
 
 
@@ -72,7 +176,7 @@ int main()
 
 	while (true)
 	{
-		cout << "Задача № ";
+		cout << "Р—Р°РґР°С‡Р° в„– ";
 		cin >> tn;
 
 switch (tn)
@@ -100,14 +204,17 @@ switch (tn)
 				
 			delete_str(a, n, m);
 
+			for (int i = 0; i < n; i++)
+				delete[] a[i];
+
 			delete[] a;
 		}
 		break;
 		case 2:
 		{
-		// 2.	Написать программу, которая поэлементно суммирует два динамических двумерных массива,
-		// заполненных случайными числами и сохраняет результат в третьем массиве.
-		// Размеры исходных массивов задаются пользователем.
+		// 2.	РќР°РїРёСЃР°С‚СЊ РїСЂРѕРіСЂР°РјРјСѓ, РєРѕС‚РѕСЂР°СЏ РїРѕСЌР»РµРјРµРЅС‚РЅРѕ СЃСѓРјРјРёСЂСѓРµС‚ РґРІР° РґРёРЅР°РјРёС‡РµСЃРєРёС… РґРІСѓРјРµСЂРЅС‹С… РјР°СЃСЃРёРІР°,
+		// Р·Р°РїРѕР»РЅРµРЅРЅС‹С… СЃР»СѓС‡Р°Р№РЅС‹РјРё С‡РёСЃР»Р°РјРё Рё СЃРѕС…СЂР°РЅСЏРµС‚ СЂРµР·СѓР»СЊС‚Р°С‚ РІ С‚СЂРµС‚СЊРµРј РјР°СЃСЃРёРІРµ.
+		// Р Р°Р·РјРµСЂС‹ РёСЃС…РѕРґРЅС‹С… РјР°СЃСЃРёРІРѕРІ Р·Р°РґР°СЋС‚СЃСЏ РїРѕР»СЊР·РѕРІР°С‚РµР»РµРј.
 			int **a, **b;
 			int na, ma, nb, mb;
 			cin >> na >> ma >> nb >> mb;
@@ -119,6 +226,27 @@ switch (tn)
 
 			for (int i = 0; i < nb; i++)
 				b[i] = new int[mb];
+
+			for (int i = 0; i < na; i++)
+			{
+				for (int j = 0; j < ma; j++)
+				{
+					a[i][j] = rand() % 10;
+					cout << a[i][j] << " ";
+				}
+				cout << endl;
+			}
+			cout << endl;
+			for (int i = 0; i < nb; i++)
+			{
+				for (int j = 0; j < mb; j++)
+				{
+					b[i][j] = rand() % 10;
+					cout << b[i][j] << " ";
+				}
+				cout << endl;
+			}
+			cout << endl;
 
 			int cs, ck;
 			if (na < nb) cs = na;
@@ -134,53 +262,104 @@ switch (tn)
 			for (int i = 0; i < cs; i++)
 			{
 				for (int j = 0; j < ck; j++)
-					cout<<
+				{
+					c[i][j] = a[i][j] + b[i][j];
+					cout << c[i][j] << " ";
+				}
+				cout << endl;
 			}
+			cout << endl;
 
+			for (int i = 0; i < na; i++)
+				delete[] a[i];
+			delete[] a;
+
+			for (int i = 0; i < nb; i++)
+				delete[] b[i];
+			delete[] b;
+
+			for (int i = 0; i < cs; i++)
+				delete[] c[i];
+			delete[] c;
 		}
 		break;
 		case 3:
 		{
+			int n;
+			cin >> n;
+			int **a;
+			a = new int*[2];
 
-		}
-		break;
-		case 4:
-		{
+			for (int i = 0; i < 2; i++)
+				a[i] = new int[n];
 
-		}
-		break;
-		case 5:
-		{
+			for (int i = 0; i < 2; i++)
+			{
+				if (i == 0) cout << "ID:   ";
+				else cout << "tel number: ";
+				for (int j = 0; j < n; j++)
+				{
+					a[0][j] = rand() % + 100;
+					a[1][j] = rand() % 1000;
+					cout << a[i][j] << "   ";
+				}
+				cout << endl;
+			}
+			cout << endl;
 
-		}
-		break;
-		case 6:
-		{
+			int choice;
+			bool f = true;
+			while (f)
+			{
+				cout << "Р’С‹Р±РµСЂРёС‚Рµ РґРµР№СЃС‚РІРёРµ:" << endl;
+				cout << "1 - РґРѕР±Р°РІР»РµРЅРёРµ РЅРѕРјРµСЂР°" << endl;
+				cout << "2 - РїРѕРёСЃРє С‚РµР»РµС„РѕРЅР° РїРѕ ID" << endl;
+				cout << "3 - РїРѕРёСЃРє С‚РµР»РµС„РѕРЅР° РїРѕ РЅРѕРјРµСЂСѓ" << endl;
+				cout << "4 - РёР·РјРµРЅРµРЅРёРµ РґР°РЅРЅС‹С…" << endl;
+				cout << "0 - РІС‹С…РѕРґ" << endl << endl;
 
-		}
-		break;
-		case 7:
-		{
+				cin >> choice;
+				switch (choice)
+				{
+				case 1:
+				{
+					input(a, n);
+					print(a, n);
+				}
+				break;
+				case 2:
+				{
+					searchID(a, n);
+					print(a, n);
+				}
+				break;
+				case 3:
+				{
+					searchNum(a, n);
+					print(a, n);
+				}
+				break;
+				case 4:
+				{
+					change(a, n);
+					print(a, n);
+				}
+				break;
+				case 0:
+				{
+					f = false;
+				}
+				break;
+				}
+			}
 
-		}
-		break;
-		case 8:
-		{
-
-		}
-		break;
-		case 9:
-		{
-
-		}
-		break;
-		case 10:
-		{
-
+			for (int i = 0; i < 2; i++)
+				delete[] a[i];
+			delete[] a;
 		}
 		break;
 		default:
-			cout << "нет такой задачи" << endl << endl;
+			cout << "РЅРµС‚ С‚Р°РєРѕР№ Р·Р°РґР°С‡Рё" << endl << endl;
 		}
 	}
 	system("pause");
